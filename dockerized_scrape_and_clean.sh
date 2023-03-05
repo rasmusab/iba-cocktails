f3909331fb53232b35b1238695b20407322e3cd2
Rasmus Bååth <rasmus.baath@gmail.com>
2023-03-05 23:52:15 +0100

    Commented docker mac hack

--- dockerized_scrape_and_clean.sh
+++ dockerized_scrape_and_clean.sh
@@ -2,6 +2,6 @@
 docker run --rm \
   -v $(pwd):/tmp/working_dir \
   -w /tmp/working_dir \
-  --platform linux/amd64 \
+  # --platform linux/amd64 \ # <- Might be needed if you're on a new Mac.
   rocker/tidyverse:4.2.2 \
   sh -c "Rscript 01_scrape.R && Rscript 02_clean.R"
